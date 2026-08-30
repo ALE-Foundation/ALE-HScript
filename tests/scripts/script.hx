72c0faac318719c8e55a3190fdf3e514395ec338
AlejoGDOfficial <alejogdoff@gmail.com>
2026-08-29 22:19:08 -0500

    te extraño script hx legible

---
+++ tests/scripts/script.hx
@@ -0,0 +1,12 @@
+var masha(default, never):String = 10;
+
+function oso(tengo:String, ?hambre:String = 'oso')
+    trace(tengo, hambre, masha);
+
+oso('quiero leche');
+
+trace('me pica la cola');
+
+oso('oso', 'tengo hambre');
+
+trace('cuando nopor vi');
